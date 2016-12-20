54810baa069a4fcae7680d265ff8512e7bc3b30b
Антон Марьин <antonaryo85@gmail.com>
2016-12-20 21:29:17 +0300

    Account properties

--- src/Account/Account.php
+++ src/Account/Account.php
@@ -2,6 +2,22 @@
 
 namespace amocrm\Account;
 
+use DateTime;
+use DateTimeZone;
+
+/**
+ * Формат даты:
+ * D	Текстовое представление дня недели, 3 символа
+ * d	День месяца, 2 цифры с ведущим нулём
+ * M	Сокращенное наименование месяца, 3 символа
+ * m	Порядковый номер месяца с ведущим нулём
+ * Y	Порядковый номер года, 4 цифры
+ * H	Часы в 24-часовом формате с ведущим нулём
+ * i	Минуты с ведущим нулём
+ * s	Секунды с ведущим нулём
+ *
+ * @package amocrm\Account
+ */
 class Account
 {
     private $id;
@@ -10,7 +26,9 @@ class Account
     private $currency;
     private $paidFrom;
     private $paidTill;
-
+    private $timezone;
+    private $datePattern;
+    private $language;
 
     /**
      * @return int
@@ -29,10 +47,58 @@ class Account
     }
 
     /**
-     * @param string $name
+     * @return string
      */
-    public function setName($name)
+    public function getSubdomain()
     {
-        $this->name = $name;
+        return $this->subdomain;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getPaidFrom()
+    {
+        return $this->paidFrom;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getPaidTill()
+    {
+        return $this->paidTill;
+    }
+
+    /**
+     * @return DateTimeZone
+     */
+    public function getTimezone()
+    {
+        return $this->timezone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDatePattern()
+    {
+        return $this->datePattern;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguage()
+    {
+        return $this->language;
     }
 }
--- tests/unit/Account/AccountTest.php
+++ tests/unit/Account/AccountTest.php
@@ -4,12 +4,85 @@ use amocrm\Account\Account;
 
 /**
  * @package amocrm\Account
+ * @coversDefaultClass amocrm\Account\Account
  */
 class AccountTest extends PHPUnit_Framework_TestCase
 {
-    public function testShouldBeAbleToCreateEmptyModel()
+
+    /**
+     * @covers ::getName
+     */
+    public function testGetName()
     {
         $model = new Account();
-        $this->assertInstanceOf('amocrm\Account\Account', $model);
+        $refObject = new ReflectionObject($model);
+        $property = $refObject->getProperty('name');
+        $property->setAccessible(true);
+        $property->setValue($model, 'some_name');
+        $this->assertEquals('some_name', $model->getName());
+    }
+
+    public function testGetId()
+    {
+        $model = new Account();
+        $refObject = new ReflectionObject($model);
+        $property = $refObject->getProperty('id');
+        $property->setAccessible(true);
+        $property->setValue($model, 123);
+        $this->assertEquals(123, $model->getId());
+    }
+
+    /**
+     * Test disabled
+     * @dataProvider propertyProvider
+     * @covers ::<public>
+     * @param string $propertyName
+     * @param mixed $value
+     */
+    public function setProperty($propertyName, $value)
+    {
+        $model = new Account();
+        $methodName = 'set' . str_replace(" ", "", ucwords(strtr($propertyName, "_-", "  ")));
+        $model->$methodName($value);
+
+        $refObject = new ReflectionObject($model);
+        $property = $refObject->getProperty($propertyName);
+        $property->setAccessible(true);
+        $this->assertEquals($value, $property->getValue($model));
+    }
+
+    /**
+     * @dataProvider propertyProvider
+     * @covers ::<public>
+     * @param string $propertyName
+     * @param mixed $value
+     */
+    public function testGetProperty($propertyName, $value)
+    {
+        $model = new Account();
+        $methodName = 'get' . str_replace(" ", "", ucwords(strtr($propertyName, "_-", "  ")));
+        $refObject = new ReflectionObject($model);
+        $property = $refObject->getProperty($propertyName);
+        $property->setAccessible(true);
+        $property->setValue($model, $value);
+        $this->assertEquals($value, $model->$methodName());
+    }
+
+    /**
+     * Провайдер для тестирования геттеров
+     * @return array [имя_свойства, значение]
+     */
+    public function propertyProvider()
+    {
+        return [
+            ['name', 'some_name'],
+            ['subdomain', 'some_domain'],
+            ['currency', 'UAH'],
+            ['paidFrom', new DateTime()],
+            ['paidTill', new DateTime()],
+            ['timezone', new DateTimeZone('Europe/Moscow')],
+            ['datePattern', 'd.m.Y H:i'],
+            ['language', 'ru'],
+        ];
     }
 }
